f5cd2c5e09b8baeba357d6810e2046667c1d4580
Paul Maddox <pmaddox@amazon.com>
2018-03-08 10:03:55 +0000

    Better CloudFormation validation output in tests

--- tests/validate-templates.sh
+++ tests/validate-templates.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 ERROR_COUNT=0; 
 
+echo "Validating AWS CloudFormation templates..."
+
 # Loop through the YAML templates in this repository
 for TEMPLATE in $(find . -name '*.yaml'); do 
 
     # Validate the template with CloudFormation
     ERRORS=$(aws cloudformation validate-template --template-body file://$TEMPLATE 2>&1 >/dev/null); 
-    
     if [ "$?" -gt "0" ]; then 
-
         ((ERROR_COUNT++));
-        echo "$TEMPLATE: $ERRORS";
+        echo "[fail] $TEMPLATE: $ERRORS";
+    else 
+        echo "[pass] $TEMPLATE";
     fi; 
     
 done; 
